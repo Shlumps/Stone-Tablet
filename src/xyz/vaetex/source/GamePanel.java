767765d544df39219019d86dd2e526b6d2133c32
Shlumps <60907573+Shlumps@users.noreply.github.com>
2020-02-26 21:36:48 -0500

    Optimizations and additions
    1. Added a keyListener for keyboard input
    2. Combined the instances mouseListener into one instance
    3. Added a repaint thread and a logical thread
    4. Casted Graphics to Graphics2D in paintComponent
    5. Cleaned up everything and added proper comments

--- src/xyz/vaetex/source/GamePanel.java
+++ src/xyz/vaetex/source/GamePanel.java
@@ -1,17 +1,44 @@
-package com.stuff;
+/*
+ * Programmar: Wyatt Rose
+ * 
+ * Last Edited: 2/26/20
+ * 
+ * Created: 2/5/20
+ * 
+ * Descripton: GamePanel is a class that helps making games easier by handling
+ * keyboad and mouse inputs as well as handling graphical updates.
+ * 
+ * Features:
+ * 	1. Easy Frame and Panel Creation
+ * 	2. Easy use of graphical updates
+ * 	3. Easy use of mouseListener methods
+ * 	4. Easy use of mouseMotionListener methods
+ * 	5. Easy use of keyListener methods
+ * 	6. Easily add your own objects by putting them in the FIELDS section
+ * 	7. A second main thread for people that don't like event based programming
+ * 	8. GamePanel is 100% better than DrawingPanel for game creation
+ * 
+ * Helpful Links:
+ * 	Key IDs: https://docs.oracle.com/en/java/javase/12/docs/api/java.desktop/java/awt/event/KeyEvent.html
+ *  Graphics2D methods: https://docs.oracle.com/en/java/javase/12/docs/api/java.desktop/java/awt/Graphics2D.html
+ */
 
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class GamePanel extends JPanel implements MouseMotionListener {
+public class GamePanel extends JPanel implements MouseMotionListener { // class that helps making games easier
 	
 	// CLASS CONSTANTS
-	private static final long serialVersionUID = 1L; // we won't be using serialization, but eclipse keeps bothering me about it
+	private static final long serialVersionUID = 1L;
+	private static final String frameName = "GamePanel";
+	private static final int panelSizeX = 800; // size of panel and frame
+	private static final int panelSizeY = 800; // ^
 	
 	// FIELDS
 	
-	// MOUSE FIELDS
+	// MOUSE AND KEY FIELDS
+	private int keyCode;
 	private int mouseX;
 	private int mouseY;
 	private int mouseButton;
@@ -19,24 +46,48 @@ public class GamePanel extends JPanel implements MouseMotionListener {
 	// CONSTRUCTORS
 	public GamePanel() {
 		
-		addMouseMotionListener(this); // GamePanel "is a" MouseMotionListener, so it can be passed as a parameter (setup for mouseMoved and mouseDragged)
+		setFocusable(true); // allows for keyboard input
 		
-		addMouseListener(new MouseAdapter() {
+		addMouseMotionListener(this); // registers this class as a mouseMotionListener
+		
+		addMouseListener(new MouseAdapter() { // mouse button methods
 			@Override
-			public void mousePressed(MouseEvent e) { // this method is called when the mouse is pressed (onClickUpdate methods should be put here)
+			public void mouseClicked(MouseEvent e) { // called on click (press and release in conjunction)
+				
+			}
+			
+			@Override
+			public void mousePressed(MouseEvent e) { // called on press
 				updateMousePosition(e);
 				updateMouseButton(e);
 			}
-		});
-		
-		addMouseListener(new MouseAdapter() {
+			
 			@Override
-			public void mouseReleased(MouseEvent e) { // this method is called when the mouse is released
+			public void mouseReleased(MouseEvent e) { // called on release
 				updateMousePosition(e);
 			}
 		});
 		
-		Thread paintThread = new Thread(new Runnable() { // anonymous instance of Runnable (this segment of code is just to call repaint continuosly)
+		addKeyListener(new KeyListener() { // keyboard input methods
+			@Override
+			public void keyTyped(KeyEvent e) { // called when typed (see java documentation for more information)
+				
+			}
+
+			@Override
+			public void keyPressed(KeyEvent e) { // called on press
+				updateKeyID(e);
+				System.out.println(e.getKeyCode());
+			}
+
+			@Override
+			public void keyReleased(KeyEvent e) { // called on release
+				
+			}
+		});
+		
+		Thread paintThread = new Thread(new Runnable() { // thread devoted to calling repaint (allows for animation)
+			@Override
 			public void run() {
 				while(true) {
 					repaint();
@@ -45,7 +96,8 @@ public class GamePanel extends JPanel implements MouseMotionListener {
 		});
 		paintThread.start();
 		
-		Thread logicalThread = new Thread(new Runnable() { // logical updates happen here
+		Thread logicalThread = new Thread(new Runnable() { // thread devoted to non-graphical processes (second main method)
+			@Override
 			public void run() {
 				while(true) {
 
@@ -54,56 +106,62 @@ public class GamePanel extends JPanel implements MouseMotionListener {
 		});
 		logicalThread.start();
 	}
-	// BEHAVIORS
+	
+	// MOUSE AND KEY METHODS
 	@Override
-	public void mouseMoved(MouseEvent e) {
+	public void mouseMoved(MouseEvent e) { // called when mouse is moved
 		updateMousePosition(e);
 	}
 	
 	@Override
-	public void mouseDragged(MouseEvent e) {
+	public void mouseDragged(MouseEvent e) { // called when mouse is pressed and moving
 		updateMousePosition(e);
 	}
 	
-	public void updateMousePosition(MouseEvent e) {
+	public void updateMousePosition(MouseEvent e) { // updates GamePanel's mouse fields
 		mouseX = e.getX();
 		mouseY = e.getY();
 	}
 	
-	public void updateMouseButton(MouseEvent e) {
+	public void updateMouseButton(MouseEvent e) { // updates GamePanel's mouseButton field
 		mouseButton = e.getButton();
 	}
 	
+	public void updateKeyID(KeyEvent e) {
+		keyCode = e.getID();
+	}
+	
 	// GRAPHICAL UPDATES
-	public void paintComponent(Graphics g) {
-		super.paintComponent(g);
-		g.setColor(Color.BLACK);
-		g.drawRect(mouseX-5, mouseY-5, 10, 10);
+	@Override
+	public void paintComponent(Graphics graphics) { // called when repaint() is called, all graphical updates are done here (refer to paintThread)
+		super.paintComponent(graphics);
+		Graphics2D g = (Graphics2D)graphics; // allows for Graphics2D painting
 	}
 	
 	// ACCESSORS
 	public int getMouseX() {return mouseX;}
 	public int getMouseY() {return mouseY;}
 	public int getMouseButton() {return mouseButton;}
-	public Dimension getPreferredSize() {return new Dimension(800,800);}
+	public int getKeyCode() {return keyCode;}
+	public Dimension getPreferredSize() {return new Dimension(panelSizeX,panelSizeY);}
 	
 	// MUTATORS
 	
-	// MAIN METHOD STUFF (TO MAKE LIFE EASIER FOR FILE TRANSPORTATION)
+	// MAIN METHOD AND FRAME CREATION
 	public static void main(String[] args) {
-		SwingUtilities.invokeLater(new Runnable() {
+		SwingUtilities.invokeLater(new Runnable() { // running this instance of runnable when the time is "just right"
 			@Override
 			public void run() {
-				createGUI("Test Panel");
+				createGUI(frameName);
 			}
 		});
 	}
 	
 	public static void createGUI(String name) {
 		JFrame f = new JFrame(name);
-		f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		f.add(new GamePanel());
-		f.pack();
+		f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); // stops the program when window is closed
+		f.add(new GamePanel()); // appends the panel to the frame
+		f.pack(); // sets the frame's dimensions to match the Dimension returned by getPrefferedSize()
 		f.setVisible(true);
 	}
 }
